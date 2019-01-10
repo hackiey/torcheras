3b6c81e2e129814ecb85d256b7479e4a5e14ed22
Harry Xie <harry_xie@live.com>
2019-01-11 02:03:12 +0800

    bug fix

--- torcheras/module.py
+++ torcheras/module.py
@@ -4,7 +4,8 @@ import json
 import shutil
 import traceback
 import torch
-import datetime
+
+from datetime import datetime
 
 from . import utils
 
@@ -13,7 +14,7 @@ class Module(torch.nn.Module):
     def __init__(self):
         super(Module, self).__init__()
 
-    def forward(self):
+    def forward(self, *inputs):
         raise NotImplementedError
         
     def compile(self,
@@ -27,7 +28,7 @@ class Module(torch.nn.Module):
         self._optimizer = optimizer
         self._loss_fn = loss_fn
         self._metrics = metrics or []
-        self._multi_masks = multi_tasks or []
+        self._multi_tasks = multi_tasks or []
 
         self._lr_scheduler = lr_scheduler
         self._custom_objects = custom_objects or {}
@@ -73,79 +74,103 @@ class Module(torch.nn.Module):
             os.mkdir(logdir)
 
         print(logdir)
+        
+        try:
+            if ema_decay:
+                ema = utils.train.EMA(ema_decay, self.parameters())
 
-        if ema_decay:
-            ema = utils.train.EMA(ema_decay, self.parameters())
+            global_step = 0
+            for epoch in range(epochs):
+                self.train()
+                train_metrics = utils.log.MetricsLog(self._metrics,
+                                                     self._loss_fn,
+                                                     multi_tasks=self._multi_tasks,
+                                                     custom_objects=self._custom_objects)
 
-        global_step = 0
-        for epoch in range(epochs):
-            self.train()
-            train_metrics = utils.log.MetricsLog(self._metrics,
-                                                 self._loss_fn,
-                                                 multi_tasks=self._multi_masks,
-                                                 custom_objects=self._custom_objects)
+                for step, batch in enumerate(train_dataloader):
+                    self._optimizer.zero_grad()
 
-            for step, batch in enumerate(train_dataloader):
-                self._optimizer.zero_grad()
+                    inputs, labels = self._variable_data(batch)
+                    preds = self.forward(*inputs)
+                    loss, batch_metrics = train_metrics.evaluate(preds, labels)
+                    # TODO: gradient accumulation
+                    loss.backward()
+                    global_step += 1
 
-                inputs, labels = self._variable_data(batch)
-                preds = self.forward(inputs)
-                loss, batch_metrics = train_metrics.evaluate(preds, labels)
-                # TODO: gradient accumulation
-                loss.backward()
-                global_step += 1
+                    if grad_clip:
+                        torch.nn.utils.clip_grad_norm_(self.parameters(), grad_clip)
 
-                if grad_clip:
-                    torch.nn.utils.clip_grad_norm_(self.parameters(), grad_clip)
+                    # TODO: lr_scheduler
+                    self._optimizer.step()
 
-                # TODO: lr_scheduler
-                self._optimizer.step()
+                    # TODO: data parallel ema
+                    if ema_decay:
+                        ema(self.named_parameters())
 
-                # TODO: data parallel ema
-                if ema_decay:
-                    ema(self.named_parameters())
+                    # batch callback
+                    if batch_callback:
+                        batch_callback(self, epoch, global_step, batch_metrics)
 
-                # batch callback
-                if batch_callback:
-                    batch_callback(self, epoch, global_step, batch_metrics)
+                    print_string, print_data = self._make_print(epoch, step, batch_metrics)
+                    print(print_string % print_data, end='')
+                    print('\r', end='')
 
-                print_string, print_data = self._make_print(epoch, step, batch_metrics)
-                print(print_string % print_data, end='')
-                print('\r', end='')
+                # train metrics
+                train_metrics_averaged = train_metrics.average()
+                print_string, print_data = self._make_print(epoch, step, train_metrics_averaged)
+                print(print_string % print_data)
 
-            # train metrics
-            train_metrics_averaged = train_metrics.average()
-            print_string, print_data = self._make_print(epoch, step, train_metrics_averaged)
-            print(print_string % print_data)
+                # epoch callback
+                if epoch_callback:
+                    epoch_callback(epoch, train_metrics_averaged)
 
-            # epoch callback
-            if epoch_callback:
-                epoch_callback(epoch, train_metrics_averaged)
+                # test
+                if test_dataloader:
+                    self.eval()
+                    test_metrics_averaged = self.evaluate(test_dataloader, 
+                                                          batch_num=len(test_dataloader),
+                                                          loss_fn=self._loss_fn,
+                                                          metrics=self._metrics,
+                                                          multi_tasks=self._multi_tasks,
+                                                          custom_objects=self._custom_objects)
 
-            # test
-            if test_dataloader:
-                self.eval()
-                test_metrics_averaged = self.evaluate(test_dataloader, batch_num=len(test_dataloader))
+                # save ema
+                if ema_decay and type(save_ema) == str:
+                    if save_ema == 'epoch':
+                        torch.save(ema.shadow, os.path.join(logdir, 'ema_'+str(epoch+1)+'.pth'))
 
-            # save ema
-            if type(save_ema) == str:
-                if save_ema == 'epoch':
-                    torch.save(ema.shadow, os.path.join(logdir, 'ema_'+str(epoch+1)+'.pth'))
+                # save checkpoint
+                if type(save_checkpoint) == str:
+                    if save_checkpoint == 'epoch':
+                        torch.save(self.state_dict(), os.path.join(logdir, 'ckpt_'+str(global_step+1)+'.pth'))
 
-            # save checkpoint
-            if type(save_checkpoint) == str:
-                if save_checkpoint == 'epoch':
-                    torch.save(self.state_dict(), os.path.join(logdir, 'ckpt_'+str(global_step+1)+'.pth'))
-
-            # save notes
-            # TODO: tensorboard
+                # save notes
+                # TODO: tensorboard
+                
+        except KeyboardInterrupt:
+            try:
+                time.sleep(0.5)
+                print("Is early stopped? please input 'y' or 'n(delete this model)'")
+                answer = input()
+                if answer == 'n':
+                    self._del_logdir(logdir)
+                elif answer == 'y':
+                    pass
+                    # self._save_notes()
+                    print('Training completed!')
+            except KeyboardInterrupt:
+                self._del_logdir(logdir)
+                
+        except Exception:
+            traceback.print_exc()
+            self._del_logdir(logdir)
 
     def evaluate(self, test_dataloader, batch_num, loss_fn=None, metrics=None, multi_tasks=None, custom_objects=None):
         with torch.no_grad():
             test_metrics = utils.log.MetricsLog(metrics, loss_fn, multi_tasks, custom_objects)
             for step, batch in enumerate(test_dataloader):
                 inputs, labels = self._variable_data(batch)
-                preds = self.model.forward(inputs)
+                preds = self.forward(*inputs)
                 test_metrics.evaluate(preds, labels)
                 if test_metrics.steps > batch_num:
                     break
@@ -158,17 +183,18 @@ class Module(torch.nn.Module):
             return test_metrics_averaged
 
     def _variable_data(self, batch):
+        device = next(self.parameters()).device
         inputs, labels = batch
         if type(inputs) is list or type(inputs) is tuple:
             for i, _inputs in enumerate(inputs):
-                inputs[i] = inputs[i].to(self.device)
+                inputs[i] = inputs[i].to(device)
         else:
-            inputs = inputs.to(self.device)
+            inputs = inputs.to(device)
         if type(labels) is list or type(labels) is tuple:
             for i, _labels in enumerate(labels):
-                labels[i] = labels[i].to(self.device)
+                labels[i] = labels[i].to(device)
         else:
-            labels = labels.to(self.device)
+            labels = labels.to(device)
 
         return inputs, labels
 
@@ -180,20 +206,24 @@ class Module(torch.nn.Module):
             print_string = '[%d, %5d] loss: %.5f'
             print_data = (epoch + 1, step + 1, metric_values['loss'])
 
-        if len(self.multi_tasks) == 0:
+        if len(self._multi_tasks) == 0:
             print_string = print_string + ', '
-            for metric in self.metrics:
+            for metric in self._metrics:
                 print_string = print_string + metric + ': %.5f, '
                 print_data = print_data + (metric_values[metric],)
             # delete last ', ' from the print string
             print_string = print_string[: -2]
         else:
-            for task in self.multi_tasks:
+            for task in self._multi_tasks:
                 print_string = print_string + ', ' + task + ': '
-                for metric in self.metrics:
+                for metric in self._metrics:
                     print_string = print_string + metric + ': %.5f, '
                     print_data = print_data + (metric_values[task][metric],)
                     # delete last ', ' from the print string
                 print_string = print_string[: -2]
 
         return print_string, print_data
+    
+    def _del_logdir(self, logdir):
+        shutil.rmtree(logdir)
+        print(logdir + ' model deleted!')
